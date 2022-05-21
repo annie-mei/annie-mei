bfd2e75e69f82ae76ca0887bb7b69189c9c25d45
InfernapeXavier <itsgood37@ymail.com>
2022-05-20 22:06:24 -0700

    Support kana search

--- src/commands/anime/fetchers/mod.rs
+++ src/commands/anime/fetchers/mod.rs
@@ -1,5 +1,6 @@
 use serde_json::json;
 use tracing::info;
+use wana_kana::{ConvertJapanese, IsJapaneseStr};
 
 use crate::utils::anilist::send_request;
 
@@ -15,10 +16,15 @@ pub fn fetch_by_id(query: String, id: u32) -> String {
 }
 
 pub fn fetch_by_name(query: String, name: String) -> String {
-    let json = json!({"query": query, "variables": {"search":name}});
+    let searchable_name = match name.is_japanese() {
+        true => name.clone().to_romaji(),
+        false => name.clone(),
+    };
+    let json = json!({"query": query, "variables": {"search":searchable_name}});
     let result: String = send_request(json);
 
-    info!("Fetched By Name: {:#?}", name);
+    info!("User input Name: {:#?}", name);
+    info!("Fetched By Name: {:#?}", searchable_name);
 
     result
 }
