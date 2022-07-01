ac91b413d402d0ff0187ef8fec989c56cf0b9d25
InfernapeXavier <itsgood37@ymail.com>
2022-07-01 15:42:07 -0700

    Working help command

--- src/commands/help.rs
+++ src/commands/help.rs
@@ -11,26 +11,31 @@ async fn help(ctx: &Context, msg: &Message) -> CommandResult {
     let msg = msg
         .channel_id
         .send_message(&ctx.http, |m| {
-            m.content("Hello, World!")
-                .embed(|e| {
-                    e.colour(0x00ff00)
-                        .title("This is a title")
-                        .description("This is a description")
-                        .image("attachment://mai.jpg")
-                        .fields(vec![
-                            ("This is the first field", "This is a field body", true),
-                            ("This is the second field", "Both fields are inline", true),
-                        ])
-                        .field(
-                            "This is the third field",
-                            "This is not an inline field",
-                            false,
-                        )
-                        .footer(|f| f.text("This is a footer"))
-                        .timestamp(chrono::Utc::now())
-                        .thumbnail("attachment://mai.jpg")
-                })
-                .add_file("./mai.jpg")
+            m.embed(|e| {
+                e.colour(0x00ff00)
+                    .title("Hello there!")
+                    .description("Use these commands to interact with Anilist!")
+                    .field(
+                        "!anime <anilist id/search term>",
+                        "Search for an anime",
+                        false,
+                    )
+                    .field(
+                        "!manga <anilist id/search term>",
+                        "Search for a manga",
+                        false,
+                    )
+                    .field(
+                        "!songs <anilist id/search term>",
+                        "Lookup the anime's songs",
+                        false,
+                    )
+                    .field("!help", "Show this message", false)
+                    .footer(|f| f.text("Annie Mai"))
+                    .timestamp(chrono::Utc::now())
+                    .thumbnail("attachment://mai.jpg")
+            })
+            .add_file("./mai.jpg")
         })
         .await;
 
