5ff0ba4f088422d4c1458f1f094b304c10b5cb54
InfernapeXavier <itsgood37@ymail.com>
2022-07-30 23:28:44 -0700

    Fix for no song #s

--- src/models/mal_response.rs
+++ src/models/mal_response.rs
@@ -27,39 +27,18 @@ struct SongInfo {
 }
 
 impl MalResponse {
-    pub fn transform_title(&self) -> String {
-        self.title.to_string()
-    }
-
-    fn transform_songs(&self, songs: Option<Vec<SongInfo>>) -> String {
-        match songs {
-            None => "No information available".to_string(),
-            Some(mut songs_list) => {
-                // Only use first 10 entries, because discord hates large embeds
-                songs_list.truncate(10);
-                songs_list.shrink_to_fit();
-                Self::format_songs_for_display(songs_list)
-            }
-        }
-    }
-
-    pub fn transform_openings(&self) -> String {
-        self.transform_songs(self.opening_themes.clone())
-    }
-
-    pub fn transform_endings(&self) -> String {
-        self.transform_songs(self.ending_themes.clone())
-    }
-
     fn format_songs_for_display(songs: Vec<SongInfo>) -> String {
         let mut return_string: Vec<String> = vec![];
         let mut parsed_songs: HashSet<u32> = HashSet::new();
-        for song in songs {
+        for (index, song) in songs.iter().enumerate() {
             let song_number = Self::get_song_number(&song.text);
-            if parsed_songs.contains(&song_number) {
-                continue;
-            } else {
-                parsed_songs.insert(song_number);
+
+            if let Some(song_number) = song_number {
+                if parsed_songs.contains(&song_number) {
+                    continue;
+                } else {
+                    parsed_songs.insert(song_number);
+                }
             }
             let song_name = Self::get_song_name(&song.text);
             let artist_names = Self::get_artist_names(&song.text);
@@ -67,8 +46,13 @@ impl MalResponse {
 
             let mut song_string = "".to_string();
 
-            // Add song number
-            write!(song_string, "{}. ", song_number).unwrap();
+            // Add song number if it exists else use index
+            write!(
+                song_string,
+                "{}. ",
+                song_number.unwrap_or_else(|| (index + 1_usize).try_into().unwrap())
+            )
+            .unwrap();
 
             // Add song name
             write!(song_string, "{}", bold(song_name)).unwrap();
@@ -86,26 +70,6 @@ impl MalResponse {
             return_string.push(song_string);
         }
         return_string.join("\n")
-    }
-
-    fn get_song_number(song: &str) -> u32 {
-        let start_index = song.find('#').unwrap();
-        let end_index = song.find(':').unwrap();
-        song[start_index + 1..end_index].parse::<u32>().unwrap()
-    }
-
-    fn get_song_name(song: &str) -> String {
-        let start_index = song
-            .find('"')
-            .unwrap_or_else(|| song.find('\'').unwrap_or(usize::MAX));
-        let end_index = song
-            .rfind('"')
-            .unwrap_or_else(|| song.rfind('\'').unwrap_or(usize::MAX));
-
-        if start_index == usize::MAX || end_index == usize::MAX {
-            return "No information available".to_string();
-        }
-        song[(start_index + 1)..end_index].to_string()
     }
 
     fn get_artist_names(song: &str) -> Option<String> {
@@ -143,6 +107,55 @@ impl MalResponse {
         Some(song[(start_index + 1)..end_index].to_string())
     }
 
+    fn get_song_name(song: &str) -> String {
+        let start_index = song
+            .find('"')
+            .unwrap_or_else(|| song.find('\'').unwrap_or(usize::MAX));
+        let end_index = song
+            .rfind('"')
+            .unwrap_or_else(|| song.rfind('\'').unwrap_or(usize::MAX));
+
+        if start_index == usize::MAX || end_index == usize::MAX {
+            return "No information available".to_string();
+        }
+        song[(start_index + 1)..end_index].to_string()
+    }
+
+    fn get_song_number(song: &str) -> Option<u32> {
+        let has_song_number = song.contains('#');
+        if !has_song_number {
+            return None;
+        }
+        let start_index = song.find('#').unwrap();
+        let end_index = song.find(':').unwrap();
+        Some(song[start_index + 1..end_index].parse::<u32>().unwrap())
+    }
+
+    pub fn transform_endings(&self) -> String {
+        self.transform_songs(self.ending_themes.clone())
+    }
+
+    pub fn transform_mal_link(&self) -> String {
+        let link = format!("https://www.myanimelist.net/anime/{}", self.id);
+        linker("MyAnimeList".to_string(), link)
+    }
+
+    pub fn transform_openings(&self) -> String {
+        self.transform_songs(self.opening_themes.clone())
+    }
+
+    fn transform_songs(&self, songs: Option<Vec<SongInfo>>) -> String {
+        match songs {
+            None => "No information available".to_string(),
+            Some(mut songs_list) => {
+                // Only use first 10 entries, because discord hates large embeds
+                songs_list.truncate(10);
+                songs_list.shrink_to_fit();
+                Self::format_songs_for_display(songs_list)
+            }
+        }
+    }
+
     pub fn transform_thumbnail(&self) -> String {
         let large = self.main_picture.large.as_ref();
         let medium = self.main_picture.medium.as_ref();
@@ -154,8 +167,7 @@ impl MalResponse {
         medium.unwrap().to_string()
     }
 
-    pub fn transform_mal_link(&self) -> String {
-        let link = format!("https://www.myanimelist.net/anime/{}", self.id);
-        linker("MyAnimeList".to_string(), link)
+    pub fn transform_title(&self) -> String {
+        self.title.to_string()
     }
 }
