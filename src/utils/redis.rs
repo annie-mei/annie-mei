491faf91f4348a001715af44ed52b913b99e71fd
InfernapeXavier <itsgood37@ymail.com>
2022-11-20 20:15:56 -0800

    new utility function for caching responses

--- src/utils/redis.rs
+++ src/utils/redis.rs
@@ -23,9 +23,20 @@ pub fn check_cache(key: &str) -> RedisResult<String> {
     Ok(cached_value)
 }
 
-pub fn cache_response(key: &str, response: &str) -> RedisResult<()> {
+fn cache_response(key: &str, response: &str) -> RedisResult<()> {
     let mut redis_client_connection = get_redis_client().unwrap();
     // Expires cached value in 5 hours
     redis_client_connection.set_ex(key, response, 18_000)?;
     Ok(())
+}
+
+pub fn try_to_cache_response(key: &str, response: &str) {
+    match cache_response(key, response) {
+        Ok(()) => {
+            info!("Successfully cached {:#?}", key);
+        }
+        Err(e) => {
+            info!("Failed to cache {:#?} with error {:#?}", key, e);
+        }
+    }
 }
