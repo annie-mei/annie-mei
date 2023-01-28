cab429811acd1cc51936b15c7439d9e8e5014e9d
InfernapeXavier <itsgood37@ymail.com>
2023-01-27 16:57:06 -0800

    static const for database url

--- src/utils/database.rs
+++ src/utils/database.rs
@@ -1,9 +1,11 @@
+use crate::utils::statics::DATABASE_URL;
+
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
 use std::env;
 
 pub fn establish_connection() -> PgConnection {
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let database_url = env::var(DATABASE_URL).expect("DATABASE_URL must be set");
     PgConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
