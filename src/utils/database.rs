9e487ac3f8da573abad745cd7dd978221bd9c6ec
InfernapeXavier <itsgood37@ymail.com>
2023-01-28 23:34:38 -0800

    setup automatic migration runs

--- src/utils/database.rs
+++ src/utils/database.rs
@@ -2,10 +2,18 @@ use crate::utils::statics::DATABASE_URL;
 
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
+use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
 use std::env;
+use tracing::info;
 
 pub fn establish_connection() -> PgConnection {
     let database_url = env::var(DATABASE_URL).expect("DATABASE_URL must be set");
     PgConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {database_url}"))
+}
+
+pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
+pub fn run_migration(conn: &mut PgConnection) {
+    info!("Running database migrations ... ");
+    conn.run_pending_migrations(MIGRATIONS).unwrap();
 }
