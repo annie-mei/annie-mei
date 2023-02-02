80451dc8536c505226e46f6ca4d207ff0ba74a53
Restyled.io <commits@restyled.io>
2023-02-02 08:17:46 +0000

    Restyled by pg_format

--- migrations/2023-02-02-075156_remove_unique_constraint_from_user/down.sql
+++ migrations/2023-02-02-075156_remove_unique_constraint_from_user/down.sql
@@ -1,4 +1,4 @@
 -- This file should undo anything in `up.sql`
 ALTER TABLE users
-  ADD CONSTRAINT users_discord_id_anilist_id_key UNIQUE (discord_id, anilist_id);
+    ADD CONSTRAINT users_discord_id_anilist_id_key UNIQUE (discord_id, anilist_id);
 
