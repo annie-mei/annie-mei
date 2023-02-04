f58dadbcfc811d7c557f8b72bd60b37ea259535d
InfernapeXavier <itsgood37@ymail.com>
2023-02-04 12:51:45 -0800

    query for user media list

--- src/utils/queries.rs
+++ src/utils/queries.rs
@@ -5,3 +5,18 @@ query ($username: String) {
   } 
 }
 ";
+
+pub const FETCH_USER_MEDIA_LIST_DATA: &str = "
+query ($userId: Int, $type: MediaType, $mediaId: Int) {
+  MediaList(userId: $userId, type: $type, mediaId: $mediaId) {
+    status
+    score(format: POINT_100)
+    progress
+    progressVolumes
+    media{
+      episodes
+      volumes
+    }
+  }
+}
+";
