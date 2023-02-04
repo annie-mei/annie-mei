bff2354af23c527ea0655b62bd4b0a28dca08b09
InfernapeXavier <itsgood37@ymail.com>
2023-02-04 14:21:04 -0800

    add guild intents

--- src/main.rs
+++ src/main.rs
@@ -161,7 +161,9 @@ async fn main() {
     let token = env::var(DISCORD_TOKEN).expect("Expected a token in the environment");
     let intents = GatewayIntents::GUILD_MESSAGES
         | GatewayIntents::DIRECT_MESSAGES
-        | GatewayIntents::MESSAGE_CONTENT;
+        | GatewayIntents::MESSAGE_CONTENT
+        | GatewayIntents::GUILD_PRESENCES
+        | GatewayIntents::GUILDS;
 
     let mut client = Client::builder(&token, intents)
         .event_handler(Handler)
