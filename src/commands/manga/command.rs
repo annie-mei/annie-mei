95bdc127a3c1b44d175eb5cfcec7b785534799d3
InfernapeXavier <itsgood37@ymail.com>
2023-02-11 15:45:08 -0800

    user scores for manga

--- src/commands/manga/command.rs
+++ src/commands/manga/command.rs
@@ -1,6 +1,10 @@
 use crate::{
     models::{anilist_manga::Manga, media_type::MediaType as Type, transformers::Transformers},
-    utils::{response_fetcher::fetcher, statics::NOT_FOUND_MANGA},
+    utils::{
+        guild::{get_current_guild_members, get_guild_scores_for_media},
+        response_fetcher::fetcher,
+        statics::NOT_FOUND_MANGA,
+    },
 };
 
 use serenity::{
@@ -45,7 +49,7 @@ pub async fn run(ctx: &Context, interaction: &mut ApplicationCommandInteraction)
         user.name
     );
 
-    let response = task::spawn_blocking(move || fetcher(Type::Manga, arg))
+    let response: Option<Manga> = task::spawn_blocking(move || fetcher(Type::Manga, arg))
         .await
         .unwrap();
 
@@ -59,6 +63,23 @@ pub async fn run(ctx: &Context, interaction: &mut ApplicationCommandInteraction)
                 .await
         }
         Some(manga_response) => {
+            // TODO: Refactor this to fetcher.rs
+
+            let guild_members = get_current_guild_members(ctx, interaction);
+            let also_manga = manga_response.clone();
+
+            if guild_members.is_empty() {
+                info!("No users found in guild")
+            } else {
+                let scores = task::spawn_blocking(move || {
+                    get_guild_scores_for_media(also_manga, guild_members)
+                })
+                .await
+                .unwrap()
+                .await;
+                info!("Guild scores: {:#?}", scores);
+            }
+
             interaction
                 .create_interaction_response(&ctx.http, |response| {
                     { response.kind(InteractionResponseType::ChannelMessageWithSource) }
